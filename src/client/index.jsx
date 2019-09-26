4d95da121657df4db93e292e3ee9afeec26b7eec
Israel Neves Micha <isrmicha@gmail.com>
2019-09-26 12:43:37 -0300

    Update index.jsx
    Removendo código comentado

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
-// import { applyMiddleware, createStore } from "redux";
-// import { Provider } from "react-redux";
-// import promise from "redux-promise";
 import thunkMiddleware from "redux-thunk";
 
 import "./index.html"
@@ -15,10 +12,6 @@ import reducers from './reducer'
 
 const store = applyMiddleware(thunkMiddleware)(createStore)(reducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-// const store = createStore(
-//   reducers,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// )
 
 ReactDOM.render(
     <Provider store={store}>
