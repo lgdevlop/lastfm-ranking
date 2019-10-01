e6bfd9afd28db7abade6ecfd65c641418683bd78
Israel Neves Micha <isrmicha@gmail.com>
2019-10-01 17:12:00 -0300

    Update express.js

--- src/server/express.js
+++ src/server/express.js
@@ -5,6 +5,8 @@ const publishing = express.static('./dist')
 
 server.use(publishing)
 
+server.use('/teste', (req, res) => res.send('teste'))
+
 server.listen(8080, () => {
   console.log('Servidor escutando na porta 8080...') // eslint-disable-line no-console
 })
